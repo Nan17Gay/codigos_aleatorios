1362011137ce22f5ef4edd14f9df11b2277cff68
Nan17Gay <usuariodedougl45@gmail.com>
2025-08-11 19:06:23 -0300

    Update epic.py

--- epic.py
+++ epic.py
@@ -6,16 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 
-# Abrir o navegador com ChromeDriverManager
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 navegador.get("https://store.epicgames.com/pt-BR/collection/top-sellers")
 
-# Esperar até os jogos carregarem
 WebDriverWait(navegador, 20).until(
     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".css-rgqwpc"))
 )
 
-# Pegar títulos e preços
 jogos = navegador.find_elements(By.CSS_SELECTOR, ".css-rgqwpc")
 precos = navegador.find_elements(By.CSS_SELECTOR, "span.eds_1ypbntd0.eds_1ypbntdc.eds_1ypbntdk.css-12s1vua")
 
@@ -31,7 +28,6 @@ for i in range(min(20, len(jogos), len(precos))):
         "preco": preco_jogo
     })
 
-# Salvar em JSON
 with open("jogos_epic.json", "w", encoding="utf-8") as arquivo:
     json.dump(dados, arquivo, ensure_ascii=False, indent=4)
 
